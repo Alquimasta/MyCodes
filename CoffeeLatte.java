9a143c72ce3983e7276b4ae7e3e38e8e6b7778e4
Alquimasta <64463325+Alquimasta@users.noreply.github.com>
2020-04-28 12:40:54 +0300

    Add files via upload

---
+++ CoffeeLatte.java
@@ -0,0 +1,12 @@
+class CoffeeLatte {
+	enum CoffeeLatteSize {SMALL, MEDIUM, LARGE}
+	CoffeeLatteSize size;
+}
+
+public class CoffeeLatteTest {
+    public static void main(String args[]) {
+		CoffeeLatte coffee = new CoffeeLatte();
+		coffe.size = CoffeeLatte.CoffeeLatteSize.SMALL ;
+		System.out.println("Size: " + coffe.size);
+	}
+}
---
+++ CycleForeach.java
@@ -0,0 +1,9 @@
+class CycleForeach {
+	public static void main(String[] args) {
+		int[] arr = {5, 4, 3, 2, 1};
+		for (int a : arr) {
+			System.out.println(a);
+		}
+	}
+}
+	
